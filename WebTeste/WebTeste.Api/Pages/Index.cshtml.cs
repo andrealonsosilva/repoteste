d1fd01def165976532345337a8adecacf665e458
André Alonso da Silva <92479896+andrealonsosilva@users.noreply.github.com>
2023-09-21 15:45:10 -0300

    Adding random (#2)
    * Adding random
    
    * teste
    
    * Update
    
    * Update sonar action file
    
    * Adicionando mensagem cagada

--- WebTeste/WebTeste.Api/Pages/Index.cshtml.cs
+++ WebTeste/WebTeste.Api/Pages/Index.cshtml.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebTeste.Api.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly Random _random;
+        private readonly string texto;
+        private readonly string MSGCAGADA = "Errooou";
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _random = new Random();
+            texto = "Teste de decoração de PR_1";
         }
 
         public void OnGet()
