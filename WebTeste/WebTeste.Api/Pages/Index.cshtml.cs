4b87d2e35c63f94b0938c0f69a700fd9912a2b3a
andrealonso93 <andre.silva@agilecontent.com>
2023-09-21 09:29:42 -0300

    Adding random

--- WebTeste/WebTeste.Api/Pages/Index.cshtml.cs
+++ WebTeste/WebTeste.Api/Pages/Index.cshtml.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebTeste.Api.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly Random _random;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _random = new Random();
         }
 
         public void OnGet()
