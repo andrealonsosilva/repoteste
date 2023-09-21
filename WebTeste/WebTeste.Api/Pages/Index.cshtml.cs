2550eeecccc511aa0e39142aba3e39d201ec8a27
andrealonso93 <andre.silva@agilecontent.com>
2023-09-21 15:21:41 -0300

    teste

--- WebTeste/WebTeste.Api/Pages/Index.cshtml.cs
+++ WebTeste/WebTeste.Api/Pages/Index.cshtml.cs
@@ -6,11 +6,12 @@ namespace WebTeste.Api.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly Random _random;
-
+        private readonly string texto;
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
             _random = new Random();
+            texto = "Teste de decoração de PR";
         }
 
         public void OnGet()
